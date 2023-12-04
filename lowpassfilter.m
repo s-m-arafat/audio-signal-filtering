df368a0a74634c29d3f1db9f377b262e1d2fd49e
Md Shakil Mahmud Arafat <31130182+s-m-arafat@users.noreply.github.com>
2023-12-05 02:42:39 +0600

    Updated lowpassfilter.m

--- lowpassfilter.m
+++ lowpassfilter.m
@@ -4,7 +4,7 @@ freq1 = 50;
 freq2 = 200;
 freq3 = 500;
 
-fs = 1500;
+fs = 1500; % 3*freq3
 t = 0:0.0005:0.1;
 
 signal1 = sin(2*pi*freq1*t);
@@ -13,12 +13,12 @@ signal3 = 10*sin(2*pi*freq3*t);
 
 combined_signal = signal1 + signal2 + signal3;
 
-cutoff_frequency = 50;
+cutoff_freq = 50;
 filter_order = 32;
 
-lpf = designfilt('lowpassfir', 'FilterOrder', filter_order, 'CutoffFrequency', cutoff_frequency, 'SampleRate', fs);
+lpf = designfilt('lowpassfir', 'FilterOrder', filter_order, 'CutoffFrequency', cutoff_freq, 'SampleRate', fs);
 % freqz(lpf);
-% Apply the filter using filtfilt
+% filtered_signal = filter(lpf, combined_signal);
 filtered_signal = filtfilt(lpf, combined_signal);
 
 figure;
@@ -31,5 +31,3 @@ title('Filtered Signal (Zero-Phase)');
 subplot(313)
 plot(t, signal1, 'g', 'LineWidth', 1.5);
 title('Signal 1');
-xlabel('Time (s)');
-ylabel('Amplitude');
